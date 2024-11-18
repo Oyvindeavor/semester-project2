a957591c30d33a3636c229d76b0c3dc529439e1a
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-19 00:05:43 +0100

    Jest config for typescript
    - Created a new file called `tsconfig.jest.json` to keep the jest required module seperate from the nextjs.
    
    - Correctly configured jest

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,21 @@
 const config = {
   preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@components/(.*)$': '<rootDir>/src/app/components/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@theme/(.*)$': '<rootDir>/src/theme/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.jest.json', // Use Jest-specific tsconfig
+      },
+    ],
   },
 };
 
