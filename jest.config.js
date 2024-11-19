f570c3b8e5aa01f2564a41d253ceb2389e274e5f
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-19 01:22:21 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,12 @@ const config = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@theme/(.*)$': '<rootDir>/src/theme/$1',
-    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
-    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@components/(.*)$': '<rootDir>/src/app/components/$1',
     '^@utils/(.*)$': '<rootDir>/src/utils/$1',
   },
   transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.jest.json', // Use Jest-specific tsconfig
-      },
-    ],
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 };
 
-export default config;
+module.exports = config;
