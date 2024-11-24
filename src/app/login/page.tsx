d4ac0e44c4565c834dc9e12465e09a39538f7054
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-24 22:56:34 +0100

    added suspense to login page
    - Added suspense to login page as it was giving an error and default to client side

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react';
 import LoginForm from '@components/forms/LoginForm';
 
 export default function LoginPage() {
   return (
     <main>
       <h1>Login</h1>
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </main>
   );
 }
