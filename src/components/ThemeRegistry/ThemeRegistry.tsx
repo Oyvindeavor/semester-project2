f05bac47f8948cdfe1dfa8146b6480774f77445c
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-26 04:18:58 +0100

    add fix for joy ui theme provider

--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -1,21 +1,67 @@
 'use client';
-import * as React from 'react';
+import createCache from '@emotion/cache';
+import { useServerInsertedHTML } from 'next/navigation';
+import { CacheProvider } from '@emotion/react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
-import NextAppDirEmotionCacheProvider from './EmotionCache';
+import React, { ReactNode } from 'react';
 import theme from './theme';
 
-export default function ThemeRegistry({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+// Define proper types for props
+interface ThemeRegistryProps {
+  options: { key: string; prepend?: boolean }; // Adjust based on your `createCache` options
+  children: ReactNode;
+}
+
+export default function ThemeRegistry(props: ThemeRegistryProps) {
+  const { options, children } = props;
+
+  const [{ cache, flush }] = React.useState(() => {
+    const cache = createCache(options);
+    cache.compat = true;
+    const prevInsert = cache.insert;
+    let inserted: string[] = [];
+    cache.insert = (...args) => {
+      const serialized = args[1];
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert(...args);
+    };
+    const flush = () => {
+      const prevInserted = inserted;
+      inserted = [];
+      return prevInserted;
+    };
+    return { cache, flush };
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) {
+      return null;
+    }
+    let styles = '';
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+    return (
+      <style
+        key={cache.key}
+        data-emotion={`${cache.key} ${names.join(' ')}`}
+        dangerouslySetInnerHTML={{
+          __html: styles,
+        }}
+      />
+    );
+  });
+
   return (
-    <NextAppDirEmotionCacheProvider options={{ key: 'joy' }}>
+    <CacheProvider value={cache}>
       <CssVarsProvider theme={theme}>
         <CssBaseline />
         {children}
       </CssVarsProvider>
-    </NextAppDirEmotionCacheProvider>
+    </CacheProvider>
   );
 }
