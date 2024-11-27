abd358b89ea650cb047abfc526301e7643df39ed
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-27 08:56:45 +0100

    added error handling to check if serverside

--- src/utils/accessToken.ts
+++ src/utils/accessToken.ts
@@ -1,18 +1,31 @@
 export function getAccessToken(): string | null {
-  if (typeof window !== 'undefined' && localStorage) {
-    return localStorage.getItem('accessToken');
+  if (typeof window !== 'undefined') {
+    try {
+      return localStorage.getItem('accessToken');
+    } catch (error) {
+      console.error('Error accessing localStorage:', error);
+      return null;
+    }
   }
-  return null; // Return null if `localStorage` is not accessible
+  return null; // Return null if not in the browser
 }
 
 export function removeAccessToken() {
-  if (typeof window !== 'undefined' && localStorage) {
-    localStorage.removeItem('accessToken');
+  if (typeof window !== 'undefined') {
+    try {
+      localStorage.removeItem('accessToken');
+    } catch (error) {
+      console.error('Error removing accessToken from localStorage:', error);
+    }
   }
 }
 
 export function storeAccessToken(token: string) {
-  if (typeof window !== 'undefined' && localStorage) {
-    localStorage.setItem('accessToken', token);
+  if (typeof window !== 'undefined') {
+    try {
+      localStorage.setItem('accessToken', token);
+    } catch (error) {
+      console.error('Error storing accessToken in localStorage:', error);
+    }
   }
 }
