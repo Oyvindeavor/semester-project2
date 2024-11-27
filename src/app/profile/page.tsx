91df2b2a6dcae4bc517bfadd512a0fc2d78ee1b0
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-27 09:19:10 +0100

    Create page.tsx

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+import Profile from '@/components/profile/profile';
+
+export default function ProfilePage() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <Profile />
+    </div>
+  );
+}
