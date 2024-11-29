955947009ac8cd0c51f316eb7465b0ca11ba2437
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-29 14:59:15 +0100

    Update navbar.tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,18 +1,40 @@
-import Link from 'next/link';
+'use client';
+
 import Box from '@mui/joy/Box';
 import AvatarNavBar from './AvatarNavBar';
 import LogoutButton from './LogoutButton';
+import { useAuth } from '@/context/useAuth';
+import StaticNavBar from './staticNavBar';
+import Link from 'next/link';
 
 export default function NavBar() {
-  return (
-    <div>
-      <Box sx={{ justifyContent: 'space-between', display: 'flex' }}>
-        <Link href={'/'}>Home</Link>
-        <Link href={`/profile/`}>Profile</Link>
-        <LogoutButton />
+  const { loggedIn } = useAuth(); // Use loggedIn from useAuth for conditional rendering
 
+  if (!loggedIn) {
+    // StaticNavBar when not logged in (faster loading server rendered)
+    return <StaticNavBar />;
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '1rem',
+        backgroundColor: 'background.level1',
+        borderBottom: '1px solid',
+        borderColor: 'divider',
+      }}
+    >
+      <Link href="/">Home</Link>
+      <div>
+        <Link href="/profile" style={{ marginRight: '1rem' }}>
+          Profile
+        </Link>
+        <LogoutButton />
         <AvatarNavBar />
-      </Box>
-    </div>
+      </div>
+    </Box>
   );
 }
