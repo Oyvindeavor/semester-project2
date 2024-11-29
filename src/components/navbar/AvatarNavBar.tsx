1c40e31589b27984a5e984c3c0ceecacd353f363
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-11-29 14:28:17 +0100

    updated avatarnavbar to not render if not logged in

--- src/components/navbar/AvatarNavBar.tsx
+++ src/components/navbar/AvatarNavBar.tsx
@@ -1,21 +1,19 @@
 'use client';
-import { Avatar, Link } from '@mui/joy';
-import { useAuth } from '@/context';
-import Dropdown from '@mui/joy';
+import { Avatar } from '@mui/joy';
+import { useAuth } from '@/context/index';
 
 export default function AvatarNavBar() {
-  const { accessToken } = useAuth();
+  const { accessToken, user } = useAuth(); // Access user and accessToken from context
 
-  // Fetch the user data from localStorage if accessToken is present
-  const user = accessToken
-    ? JSON.parse(localStorage.getItem('user') || '{}')
-    : null;
+  if (!accessToken || !user) {
+    return null; // Render nothing if the user is not logged in
+  }
 
   // Extract avatar URL and provide a fallback
   const imageUrl =
-    user?.avatar?.url ||
+    user.avatar?.url ||
     'https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=318';
-  const altText = user?.avatar?.alt || 'User Avatar';
+  const altText = user.avatar?.alt || 'User Avatar';
 
   return (
     <Avatar size="lg" sx={{ objectFit: 'contain' }}>
