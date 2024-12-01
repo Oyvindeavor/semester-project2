4719420fbf6d6394a824c08c43fca8d0f61aca92
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-12-01 14:18:14 +0100

    added create auction page
    - Added create auction page imported create auction form component

---
+++ src/app/create/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import CreateAuctionForm from '@/components/forms/createAuction';
+export default function CreatePage() {
+  return (
+    <Box>
+      <h1>Create</h1>
+      <CreateAuctionForm />
+    </Box>
+  );
+}
