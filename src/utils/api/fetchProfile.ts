4e4044f0a4b00e42e63c9de4907c3b553cf18bbd
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-12-05 13:28:57 +0100

    Update fetchProfile.ts

--- src/utils/api/fetchProfile.ts
+++ src/utils/api/fetchProfile.ts
@@ -1,34 +1,12 @@
-import { UserData } from '@/types/api/FetchProfile';
-
-export default async function fetchProfileById(
-  name: string,
-  token: string
-): Promise<UserData> {
+export async function fetchProfile() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/profile`,
-    {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'X-User-Name': name,
-      },
-    }
+    `${process.env.NEXT_PUBLIC_API_URL}/api/profile`
   );
+  const data = await response.json();
 
-  if (!response.ok) {
-    const errorBody = await response.text();
-    console.error('Error Response Body:', errorBody);
-
-    throw new Error(`Failed to fetch profile: ${response.statusText}`);
+  if (response.ok) {
+    console.log('Profile data:', data.profile);
+  } else {
+    console.error('Error fetching profile:', data.error);
   }
-
-  const jsonResponse = await response.json();
-  console.log('Received profile data:', jsonResponse);
-
-  // Extract `data` from `profile`
-  if (!jsonResponse.profile || !jsonResponse.profile.data) {
-    throw new Error('Profile data is missing in the response');
-  }
-
-  return jsonResponse.profile.data; // Return the `data` field from the `profile`
 }
