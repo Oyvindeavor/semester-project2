50e56ebe8380213c955293d99bceced87db06b1e
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-12-06 03:47:38 +0100

    test3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import Search from '@/components/Search';
 import PopularAuctionsSection from '@/components/PopularAuctionsSection';
 import EndingSoonAuctionsSection from '@/components/EndingSoonSection';
+import { API_BASE, API_KEY } from '@api/config/endpoints';
 
 console.log('[API] Starting auction listings fetch');
 console.log('[ENV_CHECK]', {
