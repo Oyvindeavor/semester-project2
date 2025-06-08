2e013ad06e0c8047778331d74cb88f2cd58ecaa7
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2025-06-08 12:07:23 +0200

    Removed unusued import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import Search from '@/components/Search';
 import EndingSoonAuctionsSection from '@/components/EndingSoonSection';
 import { Metadata } from 'next';
